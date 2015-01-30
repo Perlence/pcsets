48ae65f7a5a34bf3134a49dfbc460b74d97e4999
Sviatoslav Abakumov <dust.harvesting@gmail.com>
2015-01-31 01:30:19 +0300

    Add Python versions 2.6 and 3.3
    Tests have passed on Python 2.6 and Python 3.3.

--- setup.py
+++ setup.py
@@ -46,12 +46,15 @@ DOWNLOAD_URL = ("http://pcsets.googlecode.com/files/pcsets-%s.tar.gz" %
 
 CLASSIFIERS = """
 Development Status :: 5 - Production/Stable
+Environment :: Console
 Intended Audience :: Education
 License :: OSI Approved :: GNU General Public License (GPL)
 Programming Language :: Python
 Programming Language :: Python :: 2
+Programming Language :: Python :: 2.6
 Programming Language :: Python :: 2.7
 Programming Language :: Python :: 3
+Programming Language :: Python :: 3.3
 Programming Language :: Python :: 3.4
 Topic :: Artistic Software
 Topic :: Software Development :: Libraries :: Python Modules
